1816eec9365c0fc535aedb671b0813488673d600
Abasar-Ahmad <absarbhu2015@gmail.com>
2024-12-15 14:29:45 +0530

    Categories entity and it's crud complete

---
+++ src/main/java/com/saar/blog/payloads/CategoryDto.java
@@ -0,0 +1,23 @@
+package com.saar.blog.payloads;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class CategoryDto {
+	
+	private Integer categoryId;
+	@NotEmpty
+	@Size(min=5,message = "Write category of post minimum 5 letter")
+	private String categoryTitle;
+	@NotEmpty
+	@Size(min=12,message="write something about post description")
+	private String categoryDescription;
+}
